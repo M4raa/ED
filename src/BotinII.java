e476220fd089e6748403f6cfc6963204f8af80a2
M4raa <davidmarpons@gmail.com>
2023-12-19 11:20:22 +0100

    Update

--- src/BotinII.java
+++ src/BotinII.java
@@ -1,26 +1,40 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class BotinII {
-    public static HashMap<Integer,ArrayList<Integer>> repartir(String atraco){
-        HashMap<Integer,ArrayList<Integer>> sereparte=new HashMap<>();
-        HashMap<String,Integer> billetes=new HashMap<>();
-        String[] datos = atraco.split(",");
-        int personas = Integer.parseInt(datos[0]);
-        int cnt= 1;
-        for (int i = 1; i < datos.length; i++) {
-            billetes.put(datos[i],cnt);
+    public static HashMap<Integer,String> repartir(int participantes, int ... billetes){
+        HashMap<Integer,String> resultado = new HashMap<>();
+        ArrayList<Integer> lista = new ArrayList<>();
+        for (int billete:billetes) {
+            lista.add(billete);
         }
+        do {
+            for (int j = 0; j < participantes; j++) {
+                int Actual= lista.get(0);
+                if (resultado.containsKey(j)){
+                    resultado.replace(j, resultado.get(j) +", " + Actual);
+                }else resultado.put(j, String.valueOf(Actual));
+                lista.remove(0);
+                if (lista.isEmpty()){
+                    break;
+                }
+            }
+        } while (!lista.isEmpty());
 
-        return sereparte;
+
+        return resultado;
     }
     public static void main(String[] args) {
-        String datos;
+        /*int personas;
+        int billetes;
         Scanner sc=new Scanner(System.in);
-        System.out.println("Introduce los datos separados por una ',' :");
-        datos=sc.nextLine();
-        System.out.println(repartir(datos));
+        System.out.println("Introduce el número de participantes:");
+        personas=sc.nextLine();
+        System.out.println("Introduce los billetes a repartir:");
+        billetes=sc.nextLine();*/
+        System.out.println(repartir(4,10,20,30,40,50,60,70));
 
 
     }
