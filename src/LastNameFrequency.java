f007d324fb4c7ebd23df9c404b25792030004d9f
M4raa <davidmarpons@gmail.com>
2024-01-08 09:58:05 +0100

    Update

--- src/LastNameFrequency.java
+++ src/LastNameFrequency.java
@@ -1,33 +1,33 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class LastNameFrequency {
-    public static HashMap<String,Integer> contador(String lista) throws IOException {
+    public static String contador(String apellido) throws IOException {
         String line;
         String datos="";
+        //Leo el archivo
         BufferedReader reader = new BufferedReader(new FileReader("/home/INFORMATICA/alu10663409/Escritorio/IntJ/Estructura de datos/files/LastnameFrequencies.csv"));
         while ((line = reader.readLine())!=null) {
             datos+=line + "/n";
         }
         reader.close();
-        HashMap<String,Integer> datoss = new HashMap<>();
-        String[] lineas = lista.split("/n");
+        HashMap<String,String> lista = new HashMap<>();
+        String[] lineas = datos.split("/n");
         for (String linea:lineas) {
-            String[] dato = linea.split(",");
-            if (!datoss.containsKey(dato[6])){
-                datoss.put(dato[6],1);
-            } else {
-                datoss.replace(dato[6], datoss.get(dato[6])+1);
-            }
+            String[] cantidad=linea.split(",");
+            lista.put(cantidad[0],cantidad[1]);
         }
-        return datoss;
+
+        return lista.get(apellido);
+
     }
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        System.out.println(contador(""));
+        String dato=sc.nextLine();
+        System.out.printf("El apellido %s se repite %s veces en España",dato,contador(dato));
     }
 }
