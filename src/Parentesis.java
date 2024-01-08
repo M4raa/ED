5426133fc225b9c26079dd9daddc790212e55aca
M4raa <davidmarpons@gmail.com>
2024-01-08 10:34:27 +0100

    Update

---
+++ src/Parentesis.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Parentesis {
+    public static boolean bienFormado(String cadena){
+        Stack<Character> lista = new Stack<>();
+        for (int i = 0; i < cadena.length()-1; i++) {
+            char x=cadena.charAt(i);
+            if (x=='(' || x=='[' || x=='{'){
+                lista.add(x);
+            }else if (x==')' || x==']' || x=='}'){
+                switch (i){
+                    case ')':
+                        lista.pop()='(';
+                        break;
+
+                }
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String frase = sc.nextLine();
+        System.out.println(bienFormado(frase));
+    }
+}
